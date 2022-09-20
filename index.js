ec1def6328c7f874bd2d6ea100eb858d95101234
Keila Lopez <keilalofra@gmail.com>
2022-09-20 08:13:11 -0500

    commit tree ddd49e1c057ec4830893e07e7798794e01296a0f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "keils",
+    streetAddress: "8968 Oregon Ave"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj, 
+        [key]: value, 
+        };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    let newObj = {...obj}
+    delete newObj[key]
+    return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key]
+    return obj
+};
